587101add7ad69d0697d47f3ac4ce981995db3ef
Daniel Silva <dani3l.dxp@gmail.com>
2020-08-25 16:52:52 -0300

    feat; criando App.js para renderizar os components

---
+++ src/App/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Title from '../Components/Title';
+
+import './style.css';
+
+class App extends Component {
+	render() {
+		return <Title name="Daniel Silva" />;
+	}
+}
+
+export default App;
