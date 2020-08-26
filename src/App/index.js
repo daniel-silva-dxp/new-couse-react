e3a3d432a1e9936897997f3f63922b4913667ad8
Daniel Silva <dani3l.dxp@gmail.com>
2020-08-26 14:35:15 -0300

    feat: renderizando o component Banner

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Banner from '../Components/Banner';
 import Title from '../Components/Title';
 
 import './style.css';
 
 class App extends Component {
 	render() {
-		return <Title name="Daniel Silva" />;
+		return (
+			<div>
+				<Banner />
+				<Title name="Daniel Silva" />
+			</div>
+		);
 	}
 }
 
