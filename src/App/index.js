30a20f802f2b0115f01b02c1040987aacf60dcf6
Daniel Silva <dani3l.dxp@gmail.com>
2020-08-27 12:29:44 -0300

    feat: entendendo evendos e passando como props

--- src/App/index.js
+++ src/App/index.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import Banner from '../Components/Banner';
+import Square from '../Components/Sqaure';
 import Title from '../Components/Title';
 
 import './style.css';
 
 class App extends Component {
+	click(e) {
+		console.log(e.target);
+	}
 	render() {
 		return (
 			<div>
 				<Banner />
 				<Title name="Daniel Silva" />
+				<div>
+					<Square handleClick={this.click} />
+				</div>
 			</div>
 		);
 	}
