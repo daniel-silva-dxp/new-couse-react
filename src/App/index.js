3c3e9723fd168b751c93eb0c453d4d311df14b29
Daniel Silva <dani3l.dxp@gmail.com>
2020-08-28 14:15:53 -0300

    feat: entendendo states

--- src/App/index.js
+++ src/App/index.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 import Banner from '../Components/Banner';
-import Button from '../Components/Sqaure';
+import Button from '../Components/Button';
+import LikeButton from '../Components/LikeButton';
 import Title from '../Components/Title';
 
 import './style.css';
 
 class App extends Component {
-	click(e) {
-		console.log(e.target);
+	constructor() {
+		super();
+		this.state = {
+			name: ''
+		}
+		this.click = this.click.bind(this)
+	}
+	click() {
+		this.setState({
+			name: 'My name is Daneil Silva'
+		});
 	}
 	render() {
 		return (
 			<div>
 				<Banner />
-				<Title name="Daniel Silva" />
+				<Title name={this.state.name} />
 				<div>
 					<Button handleClick={this.click}>Click</Button>
+					<LikeButton />
 				</div>
 			</div>
 		);
