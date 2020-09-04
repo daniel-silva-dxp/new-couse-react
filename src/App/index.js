151d034690c25b04b2e9281d444f877d5c2f6857
Daniel Silva <dani3l.dxp@gmail.com>
2020-09-04 18:47:34 -0300

    feat: lifecycle part II

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Timer from '../Components/Timer';
-import NewButton from '../Components/NewButton';
 
 import './style.css';
 
@@ -8,9 +7,6 @@ class App extends Component {
 	constructor() {
 		console.log('constructor');
 		super();
-		this.state = {
-			showTimer: true
-		};
 	}
 
 	componentWillMount() {
@@ -25,10 +21,7 @@ class App extends Component {
 		console.log('render');
 		return (
 			<div className="container">
-				{this.state.showTimer && <Timer />}
-				<NewButton handleClick={() => this.setState({ showTimer: !this.state.showTimer })}>
-					Show/Hide Timer
-				</NewButton>
+				<Timer />
 			</div>
 		);
 	}
--- src/Components/Timer/index.js
+++ src/Components/Timer/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import NewButton from '../NewButton';
 
 import './style.css';
 
@@ -6,7 +7,8 @@ class Timer extends Component {
 	constructor() {
 		super();
 		this.state = {
-			timer: 0
+			timer: 0,
+			showTimer: true
 		};
 		this.timer;
 	}
@@ -23,11 +25,20 @@ class Timer extends Component {
 		clearInterval(this.timer);
 	}
 
+	componentWillReceiveProps(nextProps) {
+		console.log('componentWillReceiveProps', this.props, nextProps);
+	}
+
 	render() {
 		return (
 			<div className="box">
-				<div className="title">TIMER</div>
-				<div className="display">{this.state.timer}</div>
+				{this.state.showTimer && (
+					<div>
+						<div className="title">TIMER</div>
+						<div className="display">{this.state.timer}</div>
+					</div>
+				)}
+				<NewButton handleClick={() => this.setState({ timer: this.state.timer + 10 })}>Change Timer</NewButton>
 			</div>
 		);
 	}
