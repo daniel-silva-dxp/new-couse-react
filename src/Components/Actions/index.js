b92d11bc1a990d98dd4d3ca23d92122b72c84ffd
Daniel Silva <dani3l.dxp@gmail.com>
2020-09-07 17:09:45 -0300

    feat: criando o component Actions

---
+++ src/Components/Actions/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '../Button';
+
+import './style.css';
+
+const Actions = () => {
+	return (
+		<div className="wrap-button">
+			<Button>Repositórios</Button>
+			<Button>Favoritos</Button>
+		</div>
+	);
+};
+
+export default Actions;
