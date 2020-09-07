97505b4803eec9d87ee1c9ea4e7527c1ee9b94c4
Daniel Silva <dani3l.dxp@gmail.com>
2020-09-07 17:10:56 -0300

    feat: criando e estilizando o component repos que apresentará as informações dos repositórios do user pesquisado

---
+++ src/Components/Repos/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './style.css';
+
+const Repos = () => {
+	return (
+		<div>
+			<ul className="">
+				<li>
+					<a href="">Repos</a>
+				</li>
+				<li>
+					<a href="">Repos</a>
+				</li>
+				<li>
+					<a href="">Repos</a>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Repos;
