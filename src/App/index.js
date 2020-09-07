aa3e49b012f716e245cee9e820d1b6ec7633043d
Daniel Silva <dani3l.dxp@gmail.com>
2020-09-07 12:15:24 -0300

    feat: renderizando a page home

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Home from '../Pages/Home';
 
 import './style.css';
 
 class App extends Component {
 	render() {
-		return <div className="container" />;
+		return (
+			<div className="container">
+				<Home />
+			</div>
+		);
 	}
 }
 
