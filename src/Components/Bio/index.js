ef1bc7790d8d8dd440b9cb1aed1dfffced1d0ec4
Daniel Silva <dani3l.dxp@gmail.com>
2020-09-09 18:13:33 -0300

    feat: recebendo os states para BIO

--- src/Components/Bio/index.js
+++ src/Components/Bio/index.js
@@ -5,71 +5,88 @@ import { FaFolderOpen, FaStar, FaUserFriends, FaUserPlus } from 'react-icons/fa'
 
 import './style.css';
 
-const Bio = () => {
+const Bio = ({ showBio, userInfo, repos, starreds }) => {
 	return (
 		<div>
-			<div className="wrap-user-info wrap-align">
-				<div className="photo-user">
-					<img srcSet="http://ilustracode.com.br/assets/images/author.jpg" />
-				</div>
-				<div className="user-info">
-					<h1>
-						<a href="http://" target="_blank" rel="noopener noreferrer">
-							Daniel Silva
-						</a>
-					</h1>
-					<h4>daniel-silva-dxp</h4>
-					<div className="wrap-repos-info">
-						<ul className="repos-info">
-							<li className="margin-right-15">
-								<span className="margin-right-5">
-									<FaFolderOpen />
-								</span>
-								<span>10</span>
-							</li>
-							<li className="margin-right-15">
-								<span className="margin-right-5">
-									<FaStar />
-								</span>
-								<span>10</span>
-							</li>
-							<li className="margin-right-15">
-								<span className="margin-right-5">
-									<FaUserFriends />
-								</span>
-								<span>10</span>
-							</li>
-							<li>
-								<span className="margin-right-5">
-									<FaUserPlus />
-								</span>
-								<span>10</span>
-							</li>
-						</ul>
+			{showBio &&
+			!!userInfo && (
+				<div>
+					<div className="wrap-user-info wrap-align">
+						<div className="photo-user">
+							<img src="http://ilustracode.com.br/assets/images/author.jpg" />
+						</div>
+						<div className="user-info">
+							<h1>
+								<a href="http://" target="_blank" rel="noopener noreferrer">
+									Daniel Silva
+								</a>
+							</h1>
+							<h4>daniel-silva-dxp</h4>
+							<div className="wrap-repos-info">
+								<ul className="repos-info">
+									<li className="margin-right-15">
+										<span className="margin-right-5">
+											<FaFolderOpen />
+										</span>
+										<span>10</span>
+									</li>
+									<li className="margin-right-15">
+										<span className="margin-right-5">
+											<FaStar />
+										</span>
+										<span>10</span>
+									</li>
+									<li className="margin-right-15">
+										<span className="margin-right-5">
+											<FaUserFriends />
+										</span>
+										<span>10</span>
+									</li>
+									<li>
+										<span className="margin-right-5">
+											<FaUserPlus />
+										</span>
+										<span>10</span>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<Actions />
+					<div>
+						{!!repos.length && (
+							<Repos
+								className="repos"
+								title="Repositories"
+								repos={[
+									{ name: 'Repository I', link: '#' },
+									{ name: 'Repository II', link: '#' },
+									{ name: 'Repository III', link: '#' }
+								]}
+							/>
+						)}
+						{!!starreds.length && (
+							<Repos
+								className="starred"
+								title="Starreds"
+								repos={[
+									{ name: 'Starred I', link: '#' },
+									{ name: 'Starred II', link: '#' },
+									{ name: 'Starred III', link: '#' }
+								]}
+							/>
+						)}
 					</div>
 				</div>
-			</div>
-			<Actions />
-			<div>
-				<Repos
-					className="repos"
-					title="Repositories"
-					repos={[
-						{ name: 'Repository I', link: '#' },
-						{ name: 'Repository II', link: '#' },
-						{ name: 'Repository III', link: '#' }
-					]}
-				/>
-				<Repos
-					className="starred"
-					title="Starreds"
-					repos={[
-						{ name: 'Starred I', link: '#' },
-						{ name: 'Starred II', link: '#' },
-						{ name: 'Starred III', link: '#' }
-					]}
-				/>
-			</div>
+			)}
+			{!showBio &&
+			!!!userInfo && (
+				<div className="wrap-user-info wrap-align">
+					<div className="user-info">
+						<h1>No Record Found</h1>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
