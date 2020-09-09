2049951b9226a8fa5f161f1e4d77e028f0f6a9e7
Daniel Silva <dani3l.dxp@gmail.com>
2020-09-09 18:15:48 -0300

    feat: recebendo os staes da aplicação

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,22 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Bio from '../../Components/Bio';
 import Search from '../../Components/Search';
 
 import './style.css';
 
-export default class Home extends Component {
-	constructor() {
-		super();
-		this.state = {
-			value: ''
-		};
-	}
-	render() {
-		return (
-			<div>
-				<Search />
-				<Bio />
-			</div>
-		);
-	}
-}
+const Home = ({ showBio, userInfo, repos, starreds }) => {
+	return (
+		<div>
+			<Search labelValue="Search GitHub User" />
+			<Bio showBio={showBio} userInfo={userInfo} repos={repos} starreds={starreds} />
+		</div>
+	);
+};
+
+export default Home;
