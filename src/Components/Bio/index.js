6ba72b9d55b15911dd46f3444304f71ca9cdd68e
Daniel Silva <dani3l.dxp@gmail.com>
2020-09-10 14:08:35 -0300

    feat: deixando o component BIO dinâmico para receber os dados do user

--- src/Components/Bio/index.js
+++ src/Components/Bio/index.js
@@ -13,67 +13,48 @@ const Bio = ({ showBio, userInfo, repos, starreds }) => {
 				<div>
 					<div className="wrap-user-info wrap-align">
 						<div className="photo-user">
-							<img src="https://avatars1.githubusercontent.com/u/16245261?v=4" />
+							<img src={userInfo.avatar_url} />
 						</div>
 						<div className="user-info">
 							<h1>
-								<a href="http://" target="_blank" rel="noopener noreferrer">
-									Daniel Silva
+								<a
+									href={`https://github.com/${userInfo.login}`}
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									{userInfo.name}
 								</a>
 							</h1>
-							<h4>daniel-silva-dxp</h4>
+							<h4>{userInfo.login}</h4>
 							<div className="wrap-repos-info">
 								<ul className="repos-info">
 									<li className="margin-right-15">
 										<span className="margin-right-5">
 											<FaFolderOpen />
 										</span>
-										<span>10</span>
+										<span>{userInfo.public_repos}</span>
 									</li>
 									<li className="margin-right-15">
 										<span className="margin-right-5">
 											<FaUserFriends />
 										</span>
-										<span>10</span>
+										<span>{userInfo.followers}</span>
 									</li>
 									<li>
 										<span className="margin-right-5">
 											<FaUserPlus />
 										</span>
-										<span>10</span>
+										<span>{userInfo.following}</span>
 									</li>
 								</ul>
-								<p>
-									Developing has become a favorite hobby and eventually like to philosophize about
-									life with a beer (🍺).
-								</p>
+								<p>{userInfo.bio}</p>
 							</div>
 						</div>
 					</div>
 					<Actions />
 					<div>
-						{!!repos.length && (
-							<Repos
-								className="repos"
-								title="Repositories"
-								repos={[
-									{ name: 'Repository I', link: '#' },
-									{ name: 'Repository II', link: '#' },
-									{ name: 'Repository III', link: '#' }
-								]}
-							/>
-						)}
-						{!!starreds.length && (
-							<Repos
-								className="starred"
-								title="Starreds"
-								repos={[
-									{ name: 'Starred I', link: '#' },
-									{ name: 'Starred II', link: '#' },
-									{ name: 'Starred III', link: '#' }
-								]}
-							/>
-						)}
+						{!!repos.length && <Repos className="repos" title="Repositories" repos={repos} />}
+						{!!starreds.length && <Repos className="starred" title="Starreds" repos={starreds} />}
 					</div>
 				</div>
 			)}
